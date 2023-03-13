1bfe28f20ff95f20e68d4eef90a56d5fa57beb4a
thomastenger <118742664+thomastenger@users.noreply.github.com>
2023-03-13 10:38:28 +0100

    home-page
    ajouté la nav et le footer et la partie search dans la page accueil etudiant

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<html lang="fr">
+	<link rel="stylesheet" href="{{asset('css/style-master.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/style-welcome.css')}}">
+	<link  rel="stylesheet"href="{{asset('fontawesome/css/all.min.css')}}">
+	<link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet">
+	<title>Stage</title>
+</head>
+<body>
+	<header>	
+		<nav>
+			<ul class="menu-responsive">
+				
+				<li><a class="logo" href="#home"><img src="{{asset('css/cesi.png')}}" class="logo"></a></li>
+				<div class="responsive">
+					<li class="Recherche_Stage"><a href="#recherche">Recherche de stage</a></li>
+					<li class="icon" id="fav"><a href="#favoris" ><i class="fa-regular fa-heart"></i></a></li>
+					<span class="vertical-separator"></span>
+					<li class="icon">
+		  			<a href="#user" ><i class="fa-regular fa-user"></i></a>
+		 	 		<div class="contenu">
+						<a href="#">CHOISIR</a>
+						<a href="#">CHOISIR</a>
+						<a href="#">CHOISIR</a>
+		  			 </div>
+					</li>	
+				</div>
+				<button class="menu-hamburger"><i class="fa fa-bars"></i></button>	
+				
+	 		 </ul>
+			 
+		</nav>
+	</header>	
+	<main>
+	
+        @yield("contenu") 
+
+    </main>
+</body>
+<footer>
+	<a href="#mention" class="mention">Copyright © CESI Ecole d'Ingénieurs</a>
+</footer>
+<script>
+      const menuHamburger = document.querySelector(".menu-hamburger");
+	  const responsive = document.querySelector(".responsive");
+
+	menuHamburger.addEventListener('click', () => {
+  		responsive.classList.toggle('mobile-menu');
+});
+</script>
+</html>
