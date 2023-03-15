eaa49588423caf01dd766d22de891dcb0296a592
Mat_ <matthieu.estines@gmail.com>
2023-03-15 16:44:05 +0100

    Refractor id data

--- database/seeders/CvTableSeeder.php
+++ database/seeders/CvTableSeeder.php
@@ -15,28 +15,28 @@ class CvTableSeeder extends Seeder
     public function run()
     {
         \DB::table('cv')->insert([
-            [    
+            [
                 'file_name' => 'CV_Thomas',
                 'file_path' => 'C:\CV_Thomas.pdf',
                 'id_User' => '2',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
-            [    
+            [
                 'file_name' => 'CV_Hugo',
                 'file_path' => 'C:\CV_Hugo.pdf',
-                'id_User' => '8',
+                'id_User' => '4',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
-            [    
+            [
                 'file_name' => 'CV_Matthieu',
                 'file_path' => 'C:\CV_Mattiheu.pdf',
                 'id_User' => '1',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
-            [    
+            [
                 'file_name' => 'CV_Timothée',
                 'file_path' => 'C:\CV_Timothée.pdf',
                 'id_User' => '3',
